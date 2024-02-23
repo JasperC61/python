7357c0f638e200bfb34a3bc1b38c28685297c5ec
JasperC61 <75925282+JasperC61@users.noreply.github.com>
2024-02-23 09:31:32 +0800

    python connect to mysql
    python connect to mysql

---
+++ python-mysql-select-0223-1.py
@@ -0,0 +1,21 @@
+import mysql.connector
+#連線到資料庫
+con=mysql.connector.connect(
+    user="root",
+    password="1234",
+    host="localhost",
+    database="mydb"
+)
+
+print("資料庫連線成功")
+#建立cursor物件,來對資料庫下sql指令
+cursor=con.cursor()
+#更新資料
+productName="美式"
+productId=1
+cursor.execute("update product set name=%s where id=%s",(productName,productId))
+con.commit()
+
+con.close()
+
+#git@github.com:JasperC61/python.git
